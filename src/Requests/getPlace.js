168a0432e54e7d885906c688138d64cbfcda6ac1
Igor Ramon <igor.ramon14@gmail.com>
2021-02-27 19:41:09 -0300

    feat: render location static

--- src/Requests/getPlace.js
+++ src/Requests/getPlace.js
@@ -1,11 +1,14 @@
 import {place_key} from '../Configs/placekey';
 import placeApi from '../Services/placeApi';
 
-export default function getPlace(local) {
+export default function getPlace(local, setResults) {
   placeApi
-    .get(`json?key=${place_key}&input=${local}`, {})
+    .get(
+      `json?key=${place_key}&input=${local}&type=(cities)&components=country:br&language=pt_BR`,
+      {},
+    )
     .then((response) => {
-      console.log(response);
+      setResults(response.data.predictions);
     })
     .catch((e) => console.log(e));
 }
--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,15 +1,37 @@
-import React, {useEffect} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import getPlace from '../../Requests/getPlace';
 
 // import { Container } from './styles';
 
 function Home() {
+  const [results, setResults] = useState([]);
+
   useEffect(() => {
-    getPlace('Pau dos Ferros');
+    getPlace('Pau dos Ferros', setResults);
   }, []);
 
-  return <View />;
+  return (
+    <View>
+      <View>
+        <FlatList
+          // key={(key)=> }
+
+          keyExtractor={(item, index) => item.place_id}
+          data={results}
+          renderItem={(item) => {
+            return (
+              <View>
+                <Text>{item.item.description}</Text>
+              </View>
+            );
+          }}
+        />
+        {/* // <Text>{results[0]?.description}</Text> */}
+      </View>
+    </View>
+  );
 }
 
 export default Home;
